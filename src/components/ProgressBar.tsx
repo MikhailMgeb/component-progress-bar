f0326cc735d8594bdd70bb00d72b8008b9dae91b
Michael Mgebrishvili <mmd920802@gmail.com>
2024-03-08 15:21:42 +0300

    first commit

---
+++ src/components/ProgressBar.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import { cnProgressBar } from './ProgressBar.classname';
+
+import './ProgressBar.css';
+
+
+const ProgressBar = () => {
+    return (
+        <div className={cnProgressBar('')}>
+            
+        </div>
+    );
+}
+
+export { ProgressBar };
